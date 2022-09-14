01a2d6cb8711a27345adc7dd6a098c5539ee512d
Istalexnik <77240513+Istalexnik@users.noreply.github.com>
2022-09-14 19:21:38 -0400

    chromeprofile fix

--- GSI QA testing tool/Base.cs
+++ GSI QA testing tool/Base.cs
@@ -53,7 +53,9 @@ namespace GSI_QA_testing_tool
             chromeDriverService.HideCommandPromptWindow = true;
             var chromeOptions = new ChromeOptions();
             chromeOptions.AddArguments(new List<string>() { "no-sandbox" });
-            chromeOptions.AddArguments(new List<string>() { $@"--user-data-dir={Path.GetFullPath(@"../../Assets/ChromeProfile/")}" });
+            //keep this for the final version
+            //chromeOptions.AddArguments(new List<string>() { $@"--user-data-dir={Path.GetFullPath(@"../../Assets/ChromeProfile/")}" });
+            chromeOptions.AddArguments(new List<string>() { $@"--user-data-dir={Path.GetFullPath(@"C:/ChromeProfile/")}" });
             driver = new ChromeDriver(chromeDriverService, chromeOptions);
             driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(120);
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
