a274e881312ff7cc286de3c95835fe6298cc9775
Istalexnik <77240513+Istalexnik@users.noreply.github.com>
2022-09-14 16:46:00 -0400

    g

--- GSI QA testing tool/Base.cs
+++ GSI QA testing tool/Base.cs
@@ -10,6 +10,7 @@ using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Markup;
 using System.Windows.Threading;
 
 namespace GSI_QA_testing_tool
@@ -51,6 +52,9 @@ namespace GSI_QA_testing_tool
             chromeDriverService.HideCommandPromptWindow = true;
             var chromeOptions = new ChromeOptions();
             chromeOptions.AddArguments(new List<string>() { "no-sandbox" });
+         //   chromeOptions.AddArguments(new List<string>() { @"--user-data-dir=C:/Users/Alex/Documents/Microsoft Press/New folder/" });
+
+            
             driver = new ChromeDriver(chromeDriverService, chromeOptions);
             driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(120);
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
